4ea686a53f32dd308f6886b9b16c7b4e2fc45dc8
lawrence254 <lawrenslava1@gmail.com>
2022-02-09 13:40:47 +0300

    Complete implementation of tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  let tasks = document.getElementById('tasks');
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', handleFormSubmit);
+  function handleFormSubmit(event) {
+    let li = document.createElement('li');
+    let task = document.getElementById('new-task-description').value;
+    li.innerHTML = task;
+    tasks.appendChild(li);
+    form.reset()
+    console.log(task)
+    event.preventDefault();
+  }
 });
